6c5c65385cec3bf129c64718eb026b4c940900a5
EveLQF <65266595+evelee1234@users.noreply.github.com>
2026-04-21 17:49:29 +0800

    Update script.js

--- script.js
+++ script.js
@@ -11,14 +11,14 @@ const gifStages = [
 
 const noMessages = [
     "No",
-    "Are you positive? 🤔",
-    "Pookie please... 🥺",
-    "If you say no, I will be really sad...",
-    "I will be very sad... 😢",
-    "Please??? 💔",
-    "Don't do this to me...",
+    "Really? 🤔",
+    "Really notttt... 🥺",
+    "Are you really not...",
+    "I don't belive!... 😢",
+    "Really??? 💔",
+    "Really really...",
     "Last chance! 😭",
-    "You can't catch me anyway 😜"
+    "Bleh"
 ]
 
 const yesTeasePokes = [
